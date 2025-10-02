2f3d2ff0402f09cda7bf10a1520ca7986647712a
DiegoRomero <diego.rcorvera@gmail.com>
2025-10-02 10:45:49 -0600

    125. Valid Palindrome Diego Romero Corvera

--- algorithmic_problems/leetcode/125. Valid Palindrome/sol1.py
+++ algorithmic_problems/leetcode/125. Valid Palindrome/sol1.py
@@ -3,7 +3,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = "".join(filter(str.isalnum, s)).lower()
         for i in range(len(s)):
-            if(s[i] != s[len(s) - 1 - i]):
+            if s[i] != s[len(s) - 1 - i]:
                 return False
         return True
     
